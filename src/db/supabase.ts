92b058f38f25add75dd427bd1a526ce66f610a95
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-05-10 12:48:20 +0200

    Agregar archivo supabase.ts para la configuración del cliente Supabase

---
+++ src/db/supabase.ts
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "database.types";
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) {
+  throw new Error('Falta la URL  de Supabase')
+}
+
+if (!supabaseAnonKey) {
+  throw new Error('Falta la clave anónima de Supabase')
+}
+
+
+export const supabase = createClient<Database>(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    db:{
+      schema: 'public',
+    },
+    // auth: {
+    //   autoRefreshToken: false,
+    //   persistSession: false,
+    //   detectSessionInUrl: false
+    // }
+  }
+);
